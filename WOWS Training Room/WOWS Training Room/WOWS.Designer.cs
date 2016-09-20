a0fe0667884bdac9c05be0f547601f5534edc421
HenryQuan <natequan@hotmail.com>
2016-09-20 20:44:33 +1000

    Updating the main functions of this program.
    It is mainly about read and write an .xml file.

--- WOWS Training Room/WOWS Training Room/WOWS.Designer.cs
+++ WOWS Training Room/WOWS Training Room/WOWS.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.trainingRoom = new System.Windows.Forms.Button();
+            this.replayMode = new System.Windows.Forms.Button();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.gameDirectoryLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // trainingRoom
+            // 
+            this.trainingRoom.Location = new System.Drawing.Point(44, 34);
+            this.trainingRoom.Name = "trainingRoom";
+            this.trainingRoom.Size = new System.Drawing.Size(199, 23);
+            this.trainingRoom.TabIndex = 0;
+            this.trainingRoom.Text = "Enable Training Room";
+            this.trainingRoom.UseVisualStyleBackColor = true;
+            this.trainingRoom.Click += new System.EventHandler(this.trainingRoom_Click);
+            // 
+            // replayMode
+            // 
+            this.replayMode.Location = new System.Drawing.Point(44, 63);
+            this.replayMode.Name = "replayMode";
+            this.replayMode.Size = new System.Drawing.Size(199, 23);
+            this.replayMode.TabIndex = 1;
+            this.replayMode.Text = "Enable Replay Mode";
+            this.replayMode.UseVisualStyleBackColor = true;
+            this.replayMode.Click += new System.EventHandler(this.replayMode_Click);
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(12, 109);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(262, 20);
+            this.pathBox.TabIndex = 2;
+            // 
+            // gameDirectoryLabel
+            // 
+            this.gameDirectoryLabel.AutoSize = true;
+            this.gameDirectoryLabel.Location = new System.Drawing.Point(12, 93);
+            this.gameDirectoryLabel.Name = "gameDirectoryLabel";
+            this.gameDirectoryLabel.Size = new System.Drawing.Size(78, 13);
+            this.gameDirectoryLabel.TabIndex = 3;
+            this.gameDirectoryLabel.Text = "Game directory";
             // 
             // WOWS
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 311);
+            this.ClientSize = new System.Drawing.Size(286, 162);
+            this.Controls.Add(this.gameDirectoryLabel);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.replayMode);
+            this.Controls.Add(this.trainingRoom);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "WOWS";
             this.Text = "WOWS Training Room";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button trainingRoom;
+        private System.Windows.Forms.Button replayMode;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Label gameDirectoryLabel;
     }
 }
 
--- WOWS Training Room/WOWS Training Room/WOWS.cs
+++ WOWS Training Room/WOWS Training Room/WOWS.cs
@@ -12,9 +12,34 @@ namespace WOWS_Training_Room
 {
     public partial class WOWS : Form
     {
+
+        private string gamePath = "";
+
         public WOWS()
         {
+            // Load our saved data from Documents
+            
+
             InitializeComponent();
+        }
+
+        // Get gamePath from saved text or from test box
+        private void getGamePath()
+        {
+            gamePath = this.pathBox.Text;
+        }
+
+        private void trainingRoom_Click(object sender, EventArgs e)
+        {
+            // Get the path of game directory
+            
+
+        }
+
+        private void replayMode_Click(object sender, EventArgs e)
+        {
+            // Get the path of game directory
+
         }
     }
 }
